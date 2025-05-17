3ed9ddcd114fc057f44be1f84db27f307cd02f5a
Gustavo Marques <gustavo-markes@hotmail.com>
2025-05-17 14:47:24 -0300

    finalizando a configuração do docker

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -21,7 +21,7 @@ DATA_DIR = BASE_DIR.parent / "data" / "web"
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", 'CHANGE-ME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv("DEBUG", 0)))
