a1056c3563be445313a92f3e3aac5c500564f0a9
Gustavo Marques <gustavo-markes@hotmail.com>
2025-05-30 21:25:24 -0300

    criando um manager do Post e corrigindo erro de nome do post.cover

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,8 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django_summernote.models import AbstractAttachment
+
 from utils.images import resize_image
 from utils.rands import slugfy_new
+
+
+class PostAttachment(AbstractAttachment):
+    def save(self, *args, **kwargs):
+        if not self.name:
+            self.name = self.file.name
+
+        current_file_name = str(self.file.name)
+        super_save = super().save(*args, **kwargs)
+        file_changed = False
+
+        if self.file:
+            file_changed = current_file_name != self.file.name
+
+        if file_changed:
+            resize_image(self.file, 900, True, 70)
+
+        return super_save
 
 
 class Tag(models.Model):
@@ -82,10 +102,17 @@ class Page(models.Model):
         return self.title
 
 
+class PostManager(models.Manager):
+    def get_published(self):
+        return self.filter(is_published=True).order_by('-pk')
+
+
 class Post(models.Model):
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
+
+    objects = PostManager()
 
     title = models.CharField(max_length=65)
     slug = models.SlugField(
